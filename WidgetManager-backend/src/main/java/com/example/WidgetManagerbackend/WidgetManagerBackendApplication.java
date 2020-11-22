da3e0b724350ef0fa3aa92fa96c6f8e3698d70b3
meraihan <sayedmahmudraihan@gmail.com>
2020-11-23 02:43:27 +0600

    initial structure

---
+++ WidgetManager-backend/src/main/java/com/example/WidgetManagerbackend/WidgetManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.WidgetManagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WidgetManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WidgetManagerBackendApplication.class, args);
+	}
+
+}
---
+++ WidgetManager-backend/src/test/java/com/example/WidgetManagerbackend/WidgetManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WidgetManagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WidgetManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
