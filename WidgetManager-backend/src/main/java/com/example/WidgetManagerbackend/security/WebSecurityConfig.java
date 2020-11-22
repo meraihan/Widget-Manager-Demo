8e657f5360b52220b4eac25757dd3226650bb8cf
meraihan <sayedmahmudraihan@gmail.com>
2020-11-23 03:58:07 +0600

    endpoint changed

--- WidgetManager-backend/src/main/java/com/example/WidgetManagerbackend/security/WebSecurityConfig.java
+++ WidgetManager-backend/src/main/java/com/example/WidgetManagerbackend/security/WebSecurityConfig.java
@@ -54,7 +54,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-                .antMatchers("/api/test/**").permitAll()
+                .antMatchers("/api/publicWdgt/**").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
