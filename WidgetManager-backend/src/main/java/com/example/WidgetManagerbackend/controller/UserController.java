055ca099bd0262750d7459ddfe2e1b6fa7f42a36
meraihan <sayedmahmudraihan@gmail.com>
2020-11-23 14:13:51 +0600

    User Crud

--- WidgetManager-backend/src/main/java/com/example/WidgetManagerbackend/controller/UserController.java
+++ WidgetManager-backend/src/main/java/com/example/WidgetManagerbackend/controller/UserController.java
@@ -1,31 +1,68 @@
 package com.example.WidgetManagerbackend.controller;
 
+import com.example.WidgetManagerbackend.exception.ResourceNotFoundException;
+import com.example.WidgetManagerbackend.model.User;
+import com.example.WidgetManagerbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/publicWdgt")
 public class UserController {
 
-    @GetMapping("/all")
-    public String allAccess() {
-        return "This is a Public Content.";
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/publicWdgt")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 
-    @GetMapping("/user")
-    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
-    public String userAccess() {
-        return "This is User Content.";
+    @GetMapping("/publicWdgt/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable(value = "id") Long userId)
+            throws ResourceNotFoundException {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
+        return ResponseEntity.ok().body(user);
     }
 
-    @GetMapping("/admin")
-    @PreAuthorize("hasRole('ADMIN')")
-    public String adminAccess() {
-        return "Admin Board.";
+    @PostMapping("/publicWdgt")
+    public User createUser(@Valid @RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/publicWdgt/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable(value = "id") Long userId,
+                                                   @Valid @RequestBody User userDetails) throws ResourceNotFoundException {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
+
+        user.setUsername(userDetails.getUsername());
+        user.setEmail(userDetails.getEmail());
+        user.setPassword(userDetails.getPassword());
+        user.setRoles(userDetails.getRoles());
+        final User updatedUser = userRepository.save(user);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/publicWdgt/{id}")
+    public Map<String, Boolean> deleteUser(@PathVariable(value = "id") Long userId)
+            throws ResourceNotFoundException {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
+
+        userRepository.delete(user);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
     }
 
 }
