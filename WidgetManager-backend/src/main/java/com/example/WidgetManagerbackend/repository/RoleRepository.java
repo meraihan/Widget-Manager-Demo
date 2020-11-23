4ed97b25a46fbd59efac7e929e0815db8bce2caa
meraihan <sayedmahmudraihan@gmail.com>
2020-11-23 19:55:35 +0600

    jwt token problem solved

--- WidgetManager-backend/src/main/java/com/example/WidgetManagerbackend/repository/RoleRepository.java
+++ WidgetManager-backend/src/main/java/com/example/WidgetManagerbackend/repository/RoleRepository.java
@@ -2,9 +2,11 @@ package com.example.WidgetManagerbackend.repository;
 
 import com.example.WidgetManagerbackend.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByRoleName(Role.RoleName name);
 }
